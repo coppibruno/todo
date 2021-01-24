3f46bbcec740fe1d1c0569132d82fc04be983548
Bruno Coppi da Silva <brunocoppi130@gmail.com>
2021-01-24 15:06:28 -0300

    Componente Header

---
+++ web/web/src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import * as S from './styles';
+
+import logo from '../../assets/logo.png';
+import bell from '../../assets/bell.png';
+
+function Header() {
+  return (
+    <S.Container>
+        <S.LeftSide>
+            <img src={logo} alt="Logo" />
+        </S.LeftSide>
+        <S.RightSide>
+            <a href="#">INÍCIO</a>
+
+            <span className="dividir"></span>
+            
+            <a href="#">NOVA TAREFA</a>
+
+            <span className="dividir"></span>
+
+            <a href="#">SINCRONIZAR TAREFAS</a>
+
+            <span className="dividir"></span>
+
+            <a href="#" id="notification">
+              <img src={bell} alt="Notificação" />
+              <span>5</span>
+            </a>
+        </S.RightSide>
+    </S.Container> 
+  )
+}
+
+export default Header;
